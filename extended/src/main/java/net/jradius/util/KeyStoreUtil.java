5827998c0d1f1411475a72c1c0404931385a9a59
pphaal <peter@PETERs-iMac.local>
2018-02-21 13:18:03 -0800

    Fixed TLS certificate parsing

--- extended/src/main/java/net/jradius/util/KeyStoreUtil.java
+++ extended/src/main/java/net/jradius/util/KeyStoreUtil.java
@@ -22,11 +22,17 @@ package net.jradius.util;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.security.AlgorithmParameters;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Enumeration;
 
 import javax.net.ssl.KeyManager;
@@ -36,7 +42,19 @@ import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.util.io.pem.PemReader;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.PEMDecryptorProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMEncryptedKeyPair;
+import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.operator.InputDecryptorProvider;
 
 public class KeyStoreUtil 
 {
@@ -56,22 +74,41 @@ public class KeyStoreUtil
 
 		if (type.equalsIgnoreCase("pem"))
 		{
-			PemReader pemReader = new PemReader(new InputStreamReader(in));
-			
-			Object obj, keyObj=null, certObj=null, keyPair=null;
+			Object obj;
+                        PrivateKey key = null;
+			X509Certificate cert = null;
+			KeyPair keyPair = null; 	
 
-			while ((obj = pemReader.readPemObject()) != null)
-			{
-				if (obj instanceof X509Certificate) certObj = obj;
-				else if (obj instanceof PrivateKey) keyObj = obj;
-				else if (obj instanceof KeyPair) keyPair = obj;
+			PEMParser pemParser = new PEMParser(new InputStreamReader(in));
+			try {
+				while ((obj = pemParser.readObject()) != null)
+				{
+					if(obj instanceof X509CertificateHolder) {
+						cert = new JcaX509CertificateConverter()
+							.setProvider("BC")
+							.getCertificate((X509CertificateHolder)obj);
+					} else if(obj instanceof PrivateKeyInfo) {
+						key = BouncyCastleProvider.getPrivateKey((PrivateKeyInfo)obj);
+					} else if(obj instanceof PKCS8EncryptedPrivateKeyInfo) {
+						InputDecryptorProvider pkcs8Prov = new JceOpenSSLPKCS8DecryptorProviderBuilder().build(password.toCharArray());
+						JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+						key = converter.getPrivateKey(((PKCS8EncryptedPrivateKeyInfo)obj).decryptPrivateKeyInfo(pkcs8Prov));	
+					} else if(obj instanceof PEMKeyPair) {
+						JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+						keyPair = converter.getKeyPair((PEMKeyPair)obj);
+					} else if(obj instanceof PEMEncryptedKeyPair) {
+						PEMDecryptorProvider decProv = new JcePEMDecryptorProviderBuilder().build(password.toCharArray());
+                                                JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+    						keyPair = converter.getKeyPair(((PEMEncryptedKeyPair)obj).decryptKeyPair(decProv));
+					}
+				}
+			} finally {
+				pemParser.close();
 			}
 					
-			if ((keyObj != null || keyPair != null) && certObj != null)
+			if ((key != null || keyPair != null) && cert != null)
 			{
-				final PrivateKey key = keyPair != null ? ((KeyPair)keyPair).getPrivate() : (PrivateKey) keyObj;
-				final X509Certificate cert = (X509Certificate) certObj;
-				
+				// final PrivateKey key = keyPair != null ? ((KeyPair)keyPair).getPrivate() : (PrivateKey) keyObj;
 				KeyStore ksKeys = KeyStore.getInstance("JKS");
 				ksKeys.load(null, pwd == null ? "".toCharArray() : pwd);
 
@@ -81,35 +118,6 @@ public class KeyStoreUtil
 				kmf.init(ksKeys, pwd == null ? "".toCharArray() : pwd);
 
 				return kmf.getKeyManagers();
-				
-/*
-				return new KeyManager[] { new X509KeyManager()
-			    {
-					public String chooseClientAlias(String[] keyType, Principal[] issuers, Socket socket) {
-						return "a";
-					}
-
-					public String chooseServerAlias(String keyType, Principal[] issuers, Socket socket) {
-						return "a";
-					}
-
-					public X509Certificate[] getCertificateChain(String alias) {
-						return new X509Certificate[] { cert };
-					}
-
-					public String[] getClientAliases(String keyType, Principal[] issuers) {
-						return new String[] {"a"};
-					}
-
-					public PrivateKey getPrivateKey(String alias) {
-						return key;
-					}
-
-					public String[] getServerAliases(String keyType, Principal[] issuers) {
-						return new String[] {"a"};
-					}
-			    }};
-    */
 			}
 			else
 			{
@@ -118,32 +126,33 @@ public class KeyStoreUtil
 		}
 
 		KeyStore ksKeys = KeyStore.getInstance(type);
-        ksKeys.load(in, pwd);
+		ksKeys.load(in, pwd);
 
-        Enumeration<String> aliases = ksKeys.aliases();
+		Enumeration<String> aliases = ksKeys.aliases();
 		while (aliases.hasMoreElements()) {
 			String alias = (String) aliases.nextElement();
 			System.err.println("KeyStore Alias: "+alias);
 		}
 
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        kmf.init(ksKeys, pwd);
+        	KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        	kmf.init(ksKeys, pwd);
         
-        return kmf.getKeyManagers();
+        	return kmf.getKeyManagers();
 	}
 
 	public static X509Certificate loadCertificateFromPEM(InputStream in, final char[] pwd) throws Exception
 	{
 		loadBC();
 
-        PemReader pemReader = new PemReader(new InputStreamReader(in));
+		PEMParser pemParser = new PEMParser(new InputStreamReader(in));
 
 		Object obj;
-		while ((obj = pemReader.readPemObject()) != null)
+		while ((obj = pemParser.readObject()) != null)
 		{
-			if (obj instanceof X509Certificate)
-			{
-				return (X509Certificate) obj;
+			if(obj instanceof X509CertificateHolder) {
+				return new JcaX509CertificateConverter()
+					.setProvider("BC")
+					.getCertificate((X509CertificateHolder)obj);
 			}
 		}
 		
@@ -169,30 +178,21 @@ public class KeyStoreUtil
 			tmf.init(ksKeys);
 
 			return tmf.getTrustManagers();
-			
-/*
-			return new TrustManager[] { new X509TrustManager()
-		    {
-		        public void checkClientTrusted(X509Certificate[] chain, String authType) { }
-		        public void checkServerTrusted(X509Certificate[] chain, String authType) { }
-		        public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[] { cert }; }
-		    }};
-*/
 		}
 
 		KeyStore caKeys = KeyStore.getInstance(type);
-        caKeys.load(in, pwd);
+		caKeys.load(in, pwd);
 
-        Enumeration<String> aliases = caKeys.aliases();
+		Enumeration<String> aliases = caKeys.aliases();
 		while (aliases.hasMoreElements()) {
 			String alias = (String) aliases.nextElement();
 			System.err.println("KeyStore Alias: "+alias);
 		}
 
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        tmf.init(caKeys);
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+		tmf.init(caKeys);
         
-        return tmf.getTrustManagers();
+		return tmf.getTrustManagers();
 	}
 	
 	public static TrustManager[] trustAllManager()
